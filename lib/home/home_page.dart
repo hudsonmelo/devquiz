561dfbaae50fcbeef27e6eb8023553a1f4f52fee
Hudson Melo <hudsonmn@outlook.com>
2021-04-20 08:12:06 -0300

    Aula 01: Liftoff

---
+++ lib/home/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(),
+    );
+  }
+}
